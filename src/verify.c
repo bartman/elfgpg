df1c54884e2818a325f437a37b666d0517c8303e
Bart Trojanowski <bart@jukie.net>
2003-04-25 21:51:56 -0400

    import of elfpgp-0.1.2.tar.gz

--- src/verify.c
+++ src/verify.c
@@ -1,26 +1,24 @@
-/****************************************************************************
-* 
-* $Id: verify.c,v 1.1.1.1 2001/07/10 00:20:14 bartron Exp $
-* 
-* Copyright (C) 2001 Bart Trojanowski <bart@jukie.net>
-*
-* This file is a part of the elfverify utility
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-* 
-*****************************************************************************/
+/* 
+ * Copyright (C) 2001-2003 Bart Trojanowski <bart@jukie.net>
+ *
+ * This file is a part of the elfsign utility
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ident "%Z%%M% %I%      %E% Bart Trojanowski <bart@jukie.net>"
 
 #include <libelf.h>
 #include <sys/types.h>
@@ -455,13 +453,3 @@ do_elfverify( const char *file, int fd )
   
   return ret; 
 }
-
-
-/****************************************************************************
-* 
-* $Log: verify.c,v $
-* Revision 1.1.1.1  2001/07/10 00:20:14  bartron
-* initial import
-*
-* 
-*****************************************************************************/
